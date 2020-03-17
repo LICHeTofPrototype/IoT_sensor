5181d000ff824e48eb9266ee0fd8db45588a092c
Kojiro Machi <luxiqy24@gmail.com>
2020-03-18 00:22:44 +0900

    prototype用のコードのほぼ完成版 Remoteはシリアルプロタで確認用のコードを抜いたもの

--- ESP32_Sensor_Remote/CreateJson.ino
+++ ESP32_Sensor_Remote/CreateJson.ino
@@ -1,29 +1,22 @@
 
 #include <ArduinoJson.h>
 
-
 //***********************************
-//10ms, arrayNum = 1000のとき, MAX(strlen(buffer)) = 4028
+//10ms, arrayNum = 1000のとき, MAX(strlen(buffer)) = 5028
 int timeInterval = 10;  //測定間隔(ms)
 int arrayNum = 1000;  //配列に入れる要素数
 char buffer[5050];  //バッファ strlen(buffer)で確認できる 
-//unsigned char* buffer;
 //***********************************
-//int = arrayNum + 1;
 
 void CreateJson(){
 
-  //StaticJsonDocument<JSON_ARRAY_SIZE(5) + JSON_OBJECT_SIZE(1)> root;
-  //StaticJsonDocument<6002> root;
   DynamicJsonDocument root(20000);
   NowTime = time(NULL);
   timeInfo = localtime(&NowTime);
   sprintf(CurrentTime, "%02d:%02d:%02d", timeInfo->tm_hour, timeInfo->tm_min, timeInfo->tm_sec);
   root["time"] = CurrentTime;
-  //root["delta"] = timeInterval;
 
   JsonArray Beat = root.createNestedArray("beat");
-  Serial.println("----------------------------------------------");
   
   for(int i=1 ; i <= arrayNum; i++){
     int Signal = analogRead(SensorPin);  
@@ -31,21 +24,7 @@ void CreateJson(){
     delay(timeInterval);
   }
 
-  serializeJson(root, Serial); //シリアルモニタにjsonを表示
-  Serial.print("\n");
   serializeJson(root, buffer, sizeof(buffer)); //バッファにjsonを格納
-  Serial.println(strlen(buffer));
-  
-  //POSTはここでしてる
   int postCode = client.POST((uint8_t *) buffer, strlen(buffer));
-  
-  if( postCode == 201 ){
-    Serial.print("[---] Success on sending POST: ");
-    Serial.println(postCode); 
-  }else{
-    Serial.print("[***] Error on sending POST: ");
-    Serial.println(postCode); 
-  }
-
   Serial.flush();
 }
--- ESP32_Sensor_Remote/WiFiConnect.ino
+++ ESP32_Sensor_Remote/WiFiConnect.ino
@@ -8,38 +8,27 @@ void WiFiConnect(){
     count = 0;
 
     while (WiFi.status() != WL_CONNECTED){
-
       if(count == 5){
-        Serial.print("\n");
-        Serial.println("[!!!] Try Connecting WiFi again");
         break;
       }else if (count % 2 == 0){
         digitalWrite(WiFiPin, LOW);
       }else {
         digitalWrite(WiFiPin, HIGH);
       }
-      
-      Serial.print(".");
       delay(1000);
-      count += 1;
-      
+      count += 1;    
     }
 
     if (WiFi.status() == WL_CONNECTED){
       digitalWrite(WiFiPin, HIGH);
-      Serial.print("\n");
-      Serial.println("[---] WiFi Connected");
-
       configTime( JST, 0, "ntp.nict.jp", "ntp.jst.mfeed.ad.jp");
       break;
     }
-  }
-     
+  }   
 }
 
 
 void WiFiDisConnect() {
   WiFi.disconnect();
   digitalWrite(WiFiPin, LOW);
-  Serial.println("[---] WiFi Disconnected");
 }
